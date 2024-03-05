dbe5410cbf9b6d5b124fe73ad8de1fc074c9746e
Shrutika Malve <113254896+ShrutikaM25@users.noreply.github.com>
2024-03-05 23:18:44 +0530

    Update payment.js

--- src/pages/Subcriptions/payment.js
+++ src/pages/Subcriptions/payment.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import StripeCheckout from 'react-stripe-checkout';
 import './index.css'; 
 import config from '../../config.js';
+import axios from 'axios';
+import BACK_URL from '../../helper';
+import auth from '../../firebase.init';
+import  {useAuthState } from 'react-firebase-hooks/auth';
 
 const Payment = () => {
   const { plan, price } = useParams();
+  const user = useAuthState(auth);
+  const email = user[0].email;
+  const [PaymentComplete, setPaymentComplete] = useState(false);
   const onToken = (token) => {
     console.log(token);
+    setPaymentComplete(true);
+    axios.patch(`${BACK_URL}/userUpdates/${email}`, {subscriptionPlan:plan})
 
-    const paymentSuccessful = true;
+    const paymentSuccessful = false;
 
     if (paymentSuccessful) {
       toast.success('Payment successful!');
@@ -22,7 +31,7 @@ const Payment = () => {
 
   return (
     <div className='payment'>
-      <h2>Confirm Your Payment</h2>
+      <h2 style={{color:"black"}}>Confirm Your Payment</h2>
       {plan ? (
         <div className='selected-plan'>
           <p>You have selected the {plan}</p>
