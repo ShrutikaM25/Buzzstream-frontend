39598ffbbafe85097f631d09df5bb56454a2856f
Shrutika Malve <113254896+ShrutikaM25@users.noreply.github.com>
2024-03-05 22:53:40 +0530

    Update Feed.js

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -3,20 +3,22 @@ import '../Page.css';
 import TweetBox from './TweetBox/TweetBox';
 import Post from './Post/Post';
 import './Feed.css'
+import BACK_URL from '../../helper';
 
 
 const Feed = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-   fetch('http://localhost:5000/post') 
+   fetch(`${BACK_URL}/post`) 
    .then(res => res.json())
    .then(data =>{
     setPosts(data)
    })
   },[posts])
-  return (
+     return (
     <div>
+     
       <TweetBox/>
       {
         posts.map(p=><Post key = {p._id} p={p}/>)
